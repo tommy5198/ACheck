6c659ad72c863d2115cf027520a22310879160cb
TZY Chiu <tzychiu@TZYdeMacBook-Air.local>
2015-03-16 15:49:52 +0800

    uva checker completed

--- ACheck.py
+++ ACheck.py
@@ -1,35 +1,45 @@
 import urllib2
+import os
 import json
 
+pset = {}
+
 def ACheck():
+    global pset
     uid = ""
     uname = []
 
     with open('uname.txt', 'r') as f:
         for username in f:
-uname.append(str(username))
-        uid += str(json.load(urllib2.urlopen('http://uhunt.felix-halim.net/api/uname2uid/'  + username))) + ','
+            uname.append(str(username))
+            uid += str(json.load(urllib2.urlopen('http://uhunt.felix-halim.net/api/uname2uid/'  + username))) + ','
 
     result = json.load(urllib2.urlopen('http://uhunt.felix-halim.net/api/solved-bits/' + uid ))
-
     cnt = 0
 
     for user in result:
-        count = 0
-        for bits in user['solved']:
-            while bits != 0:
-                count += bits & 1
-                bits = bits >> 1
-        print uname[cnt][:-1] + ': ' + str(count)
+        print uname[cnt][:-1] + ': '
+        total = 0
+        for hw in pset.keys():
+            count = 0
+            for pid in pset[hw]:
+                i = pid % 32
+                j = (pid - i) / 32
+                if len(user['solved']) > j:
+                    count += (user['solved'][j] >> i) & 1
+            print str(count) + ' '
+            total += count
         cnt += 1
+        print str(total)
 
-def addPid(filename):
+def addPid():
+    global pset
+    filenames = [f for f in os.listdir('hw/')]
+    for hw in filenames:
+        pset[hw] = []
+        with open('hw/' + hw, 'r') as f:
+            for pnum in f:
+                pset[hw].append(json.load(urllib2.urlopen('http://uhunt.felix-halim.net/api/p/num/' + pnum))['pid'])
 
-    psetf = open('pset.txt')
-    for subset in psetf:
-        pset.append(subset)
-    with open(filename, 'r') as f:
-        for pnum in f:
-            out.write(json.load(urllib2.urlopen('http://uhunt/felix=halim.net/api/p/num/' + pnum))['num'] + ' ')
-        
-            
+addPid()
+ACheck()
